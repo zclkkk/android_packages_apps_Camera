0e3ce376db33bbfdd20111a30498ab5dc0cc9db8
zclkkk <2484392714@qq.com>
2022-06-25 18:38:26 +0800

    Camera: Rebrand for Project-Kaleidoscope
    Change-Id: I5b3ca2116cf457e146640d2bcc5ce59abbcd5a3b

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
     buildToolsVersion = "32.0.0"
 
     defaultConfig {
-        applicationId = "app.grapheneos.camera"
+        applicationId = "ink.kaleidoscope.camera"
         minSdk = 29
         targetSdk = 32
         versionCode = 43
